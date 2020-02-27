2fee9bffd05e95e1271ac0d49983a73e83969746
Omar Amara <O_amara@hotmail.com>
2020-02-27 17:11:48 -0600

    Set-up ClientLoginRegisterForm component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import ClientLoginRegisterForm from './ClientLoginRegisterForm'
 
 function App() {
   return (
     <div className="App">
-   
+   		<ClientLoginRegisterForm />
     </div>
   );
 }
---
+++ src/ClientLoginRegisterForm/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class ClientLoginRegisterForm extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+
+		}
+	}
+
+	render() {
+		return(
+			<React.Fragment>
+				<h1>Form Rendering</h1>
+			</React.Fragment>
+		)
+	}
+}
