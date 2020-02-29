dcedc0762fc3a3b66b489b5e3a7fd34934d007e4
Omar Amara <O_amara@hotmail.com>
2020-02-28 23:50:09 -0600

    Realtor Register Form Complete

--- src/RealtorLoginRegisterForm/index.js
+++ src/RealtorLoginRegisterForm/index.js
@@ -12,6 +12,17 @@ export default function RealtorLoginRegisterForm(props) {
 					<Form onSubmit={() => props.handleAllFormSubmission('register')}>
 						<Form.Group widths='equal'>
 							<Form.Field>
+								<Label className='Label'>Email:</Label>
+								<Input
+									required
+									type='email'
+									name='email'
+									placeholder='Enter Email'
+									value={props.registerForm.email}
+									onChange={props.handleRegisterFormChange}
+								/>
+							</Form.Field>
+							<Form.Field>
 								<Label className='Label'>Username:</Label>
 								<Input
 									required
@@ -21,7 +32,7 @@ export default function RealtorLoginRegisterForm(props) {
 									value={props.registerForm.username}
 									onChange={props.handleRegisterFormChange}
 								/>
-								</Form.Field>
+							</Form.Field>
 								<Form.Field>
 								<Label className='Label'>Password:</Label>
 								<Input
@@ -33,8 +44,138 @@ export default function RealtorLoginRegisterForm(props) {
 									onChange={props.handleRegisterFormChange}
 								/>
 							</Form.Field>
-							<Button color={'vk'} type='Submit'>Register</Button>
 						</Form.Group>
+						<Form.Group widths='equal'>
+							<Form.Field>
+								<Label className='Label'>First Name:</Label>
+								<Input
+									required
+									type='text'
+									name='firstName'
+									placeholder='Your First Name'
+									value={props.registerForm.firstName}
+									onChange={props.handleRegisterFormChange}
+								/>
+							</Form.Field>
+							<Form.Field>
+								<Label className='Label'>Last Name:</Label>
+								<Input
+									required
+									type='text'
+									name='lastName'
+									placeholder='Your Last Name'
+									value={props.registerForm.lastName}
+									onChange={props.handleRegisterFormChange}
+								/>
+							</Form.Field>
+							<Form.Field>
+								<Label className='Label'>Phone Number:</Label>
+								<Input
+									required
+									type='tel'
+									name='phoneNumber'
+									placeholder='Enter Phone Number'
+									value={props.registerForm.phoneNumber}
+									onChange={props.handleRegisterFormChange}
+									maxLength='11'
+								/>
+							</Form.Field>
+						</Form.Group>
+						<Form.Group widths='equal'>
+							<Form.Field>
+								<Label className='Label'>Broker License Number:</Label>
+								<Input
+									required
+									type='text'
+									name='brokerLicenseNumber'
+									placeholder='Broker License #'
+									value={props.registerForm.brokerLicenseNumber}
+									onChange={props.handleRegisterFormChange}
+								/>
+							</Form.Field>
+							<Form.Field>
+								<Label className='Label'>Website Link:</Label>
+								<Input
+									type='url'
+									name='websiteURL'
+									placeholder='Website Link'
+									value={props.registerForm.websiteURL}
+									onChange={props.handleRegisterFormChange}
+								/>
+							</Form.Field>
+						</Form.Group>
+						<Form.Group widths='equal'>
+							<Form.Field>
+								<Label className='Label'>Company Name:</Label>
+								<Input
+									required
+									type='text'
+									name='companyName'
+									placeholder='Your Company Name'
+									value={props.registerForm.companyName}
+									onChange={props.handleRegisterFormChange}
+								/>
+							</Form.Field>
+							<Form.Field>
+								<Label className='Label'>Company Number:</Label>
+								<Input
+									required
+									type='text'
+									name='companyNumber'
+									placeholder='Compnay Number'
+									value={props.registerForm.lastName}
+									onChange={props.handleRegisterFormChange}
+								/>
+							</Form.Field>
+							<Form.Field>
+								<Label className='Label'>Street 1:</Label>
+								<Input
+									required
+									type='text'
+									name='street1'
+									placeholder='Company Street Number'
+									value={props.registerForm.street1}
+									onChange={props.handleRegisterFormChange}
+								/>
+							</Form.Field>
+						</Form.Group>
+							<Form.Group widths='equal'>
+							<Form.Field>
+								<Label className='Label'>City:</Label>
+								<Input
+									required
+									type='text'
+									name='city'
+									placeholder="Your Company's City"
+									value={props.registerForm.city}
+									onChange={props.handleRegisterFormChange}
+								/>
+							</Form.Field>
+							<Form.Field>
+								<Label className='Label'>State:</Label>
+								<Input
+									required
+									type='text'
+									name='state'
+									placeholder='State'
+									value={props.registerForm.state}
+									onChange={props.handleRegisterFormChange}
+								/>
+							</Form.Field>
+							<Form.Field>
+								<Label className='Label'>Zipcode:</Label>
+								<Input
+									required
+									type='number'
+									name='zipcode'
+									placeholder='Zipcode'
+									maxLength='10'
+									value={props.registerForm.zipcode}
+									onChange={props.handleRegisterFormChange}
+								/>
+							</Form.Field>
+						</Form.Group>
+						<Button color={'vk'} type='Submit'>Register</Button>
 					</Form>
 				</div>
 				<div className='About-Us'>
@@ -81,28 +222,4 @@ export default function RealtorLoginRegisterForm(props) {
 			</div>
 		</React.Fragment>
 	)
-}/*
-	company: {
-		name: {
-		},
-		location: {
-			street1: {
-			},
-			street2: {
-			},
-			city: {
-			},
-			state: {
-			},
-			zipcode: {
-			}
-		},
-		phone: {
-			minlength: 10,
-		}
-	},
-	username:	firstName:   lastName:   email:   brokerLicenseNumber:    websiteURL:    password:
-	phoneNumber: {
-		minlength: 10,
-	},
-*/
+}
